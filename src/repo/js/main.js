3923d7e603f69895ad0d2787b2146793cf2a2bb1
Ivanq <imachug@yandex.ru>
2017-09-09 21:52:24 +0300

    Add rename feature

--- src/repo/js/main.js
+++ src/repo/js/main.js
@@ -1,8 +1,34 @@
 let zeroFrame = new ZeroFrame();
 let zeroPage = new ZeroPage(zeroFrame);
 
-let repo = location.search.replace(/[?&]wrapper_nonce=.*/, "").replace("?", "");
-if(!repo) {
+let address = location.search.replace(/[?&]wrapper_nonce=.*/, "").replace("?", "");
+if(!address) {
 	location.href = "..";
 }
-console.log(repo);
+
+let repo = new Repository(address, zeroPage);
+
+function showTitle(title) {
+	let name = document.getElementById("repo_name");
+	name.textContent = title;
+	name.innerHTML += document.getElementById("edit_icon_tmpl").innerHTML;
+
+	document.getElementById("edit_icon").onclick = renameRepo;
+}
+
+function renameRepo() {
+	let newName;
+	return zeroPage.prompt("New name:")
+		.then(n => {
+			newName = n;
+
+			return repo.rename(newName);
+		})
+		.then(() => showTitle(newName));
+}
+
+repo.addMerger()
+	.then(() => {
+		return repo.getContent();
+	})
+	.then(content => showTitle(content.title));
