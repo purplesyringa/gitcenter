824e2e30a38c803622e4d5a1b940c08703f45e12
Ivanq <imachug@yandex.ru>
2017-09-13 20:23:18 +0300

    Add support for branches for /repo/file

--- src/repo/file/js/main.js
+++ src/repo/file/js/main.js
@@ -12,6 +12,15 @@ if(address.indexOf("/") > -1) {
 	address = address.substr(0, address.indexOf("/"));
 }
 
+let branch = "master";
+if(path.indexOf("@") > -1) {
+	let tempPath = path.replace(/@@/g, "\0"); // @ is escaped
+	path = tempPath.substr(0, tempPath.indexOf("@")).replace(/\0/g, "@");
+	branch = tempPath.substr(tempPath.indexOf("@") + 1).replace(/\0/g, "@");
+} else {
+	path = path.replace(/@@/g, "@");
+}
+
 let repo = new Repository(address, zeroPage);
 
 function showTitle(title) {
@@ -20,6 +29,46 @@ function showTitle(title) {
 	name.innerHTML += document.getElementById("edit_icon_tmpl").innerHTML;
 
 	document.getElementById("edit_icon").onclick = renameRepo;
+}
+
+function showBranches() {
+	return repo.getBranches()
+		.then(list => {
+			// Show branch list
+			let branches = document.getElementById("branches");
+			list.forEach(branch => {
+				let option = document.createElement("option");
+				option.textContent = branch;
+				branches.appendChild(option);
+			});
+
+			branches.value = branch;
+
+			branches.onchange = () => {
+				location.href = "?" + address + "/" + path.replace(/@/g, "@@") + "@" + branches.value.replace(/@/g, "@@");
+			};
+		});
+}
+
+function showPath() {
+	// Show path
+	document.getElementById("files_root").href = "../?" + address;
+
+	let filesPath = document.getElementById("files_path");
+	let parts = path.split("/").filter(part => part.length);
+	parts.forEach((part, i) => {
+		let node = document.createElement("span");
+		node.textContent = i == parts.length - 1 ? "" : " › ";
+
+		let link = document.createElement(i == parts.length - 1 ? "span" : "a");
+		link.textContent = part;
+		if(i < parts.length - 1) {
+			link.href = "../?" + address + "/" + parts.slice(0, i + 1).join("/");
+		}
+		node.insertBefore(link, node.firstChild);
+
+		filesPath.appendChild(node);
+	});
 }
 
 function renameRepo() {
@@ -39,41 +88,25 @@ repo.addMerger()
 	})
 	.then(content => {
 		showTitle(content.title);
+		showBranches();
+		showPath();
 
-		return repo.getFile("master", path);
+		return repo.getFiles(branch, "");
 	})
-	.then(blob => {
+	.then(() => {
+		// Tree exists
+		return repo.getFile(branch, path)
+			.then(blob => {
+				let fileContent = document.getElementById("file_content");
+				fileContent.textContent = repo.git.arrayToString(blob);
+				hljs.highlightBlock(fileContent);
+			}, () => {
+				// Blob doesn't exist
+				let fileContent = document.getElementById("file_content");
+				fileContent.textContent = "File " + path + " does not exist on branch " + branch;
+			});
+	}, () => {
+		// Tree doesn't exist
 		let fileContent = document.getElementById("file_content");
-		fileContent.textContent = repo.git.arrayToString(blob);
-		hljs.highlightBlock(fileContent);
-
-		// Show path
-		document.getElementById("files_root").href = "../?" + address;
-
-		let filesPath = document.getElementById("files_path");
-		let parts = path.split("/").filter(part => part.length);
-		parts.forEach((part, i) => {
-			let node = document.createElement("span");
-			node.textContent = i == parts.length - 1 ? "" : " › ";
-
-			let link = document.createElement(i == parts.length - 1 ? "span" : "a");
-			link.textContent = part;
-			if(i < parts.length - 1) {
-				link.href = "../?" + address + "/" + parts.slice(0, i + 1).join("/");
-			}
-			node.insertBefore(link, node.firstChild);
-
-			filesPath.appendChild(node);
-		});
-
-		return repo.getBranches();
-	})
-	.then(list => {
-		// Show branch list
-		let branches = document.getElementById("branches");
-		list.forEach(branch => {
-			let option = document.createElement("option");
-			option.textContent = branch;
-			branches.appendChild(option);
-		});
+		fileContent.textContent = "Unknown branch " + branch;
 	});
