e57e3426524b89f2d365fa35e275c8673c099c02
Ivanq <imachug@yandex.ru>
2017-09-13 17:14:04 +0300

    Repo: Show file list

--- src/repo/js/main.js
+++ src/repo/js/main.js
@@ -6,6 +6,12 @@ if(!address) {
 	location.href = "..";
 }
 
+let path = "";
+if(address.indexOf("/") > -1) {
+	path = address.substr(address.indexOf("/") + 1);
+	address = address.substr(0, address.indexOf("/"));
+}
+
 let repo = new Repository(address, zeroPage);
 
 function showTitle(title) {
@@ -31,4 +37,27 @@ repo.addMerger()
 	.then(() => {
 		return repo.getContent();
 	})
-	.then(content => showTitle(content.title));
+	.then(content => {
+		showTitle(content.title);
+
+		return repo.getFiles("master", path);
+	})
+	.then(files => {
+		let table = document.getElementById("files");
+		files
+			.sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
+			.forEach(file => {
+				let tr = document.createElement("tr");
+
+				let name = document.createElement("td");
+				name.textContent = file.name;
+				tr.appendChild(name);
+
+				let icon = document.createElement("img");
+				icon.className = "file-icon";
+				icon.src = "../img/" + file.type + ".svg";
+				name.insertBefore(icon, name.firstChild);
+
+				table.appendChild(tr);
+			});
+	});
