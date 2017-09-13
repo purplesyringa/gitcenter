e062f03fb8a57c6f00f25423fd5b4dd844c1bb56
Ivanq <imachug@yandex.ru>
2017-09-13 20:36:43 +0300

    CommonJS: Move address/path/branch/repo definitions to common.js

--- src/repo/file/js/main.js
+++ src/repo/file/js/main.js
@@ -1,28 +1,3 @@
-let zeroFrame = new ZeroFrame();
-let zeroPage = new ZeroPage(zeroFrame);
-
-let address = location.search.replace(/[?&]wrapper_nonce=.*/, "").replace("?", "");
-if(!address) {
-	location.href = "..";
-}
-
-let path = "";
-if(address.indexOf("/") > -1) {
-	path = address.substr(address.indexOf("/") + 1);
-	address = address.substr(0, address.indexOf("/"));
-}
-
-let branch = "master";
-if(path.indexOf("@") > -1) {
-	let tempPath = path.replace(/@@/g, "\0"); // @ is escaped
-	path = tempPath.substr(0, tempPath.indexOf("@")).replace(/\0/g, "@");
-	branch = tempPath.substr(tempPath.indexOf("@") + 1).replace(/\0/g, "@");
-} else {
-	path = path.replace(/@@/g, "@");
-}
-
-let repo = new Repository(address, zeroPage);
-
 repo.addMerger()
 	.then(() => {
 		return repo.getContent();
