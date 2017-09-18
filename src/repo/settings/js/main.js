3419d2155d0e5e7fb7704cfcbdfbae236ab94c74
Ivanq <imachug@yandex.ru>
2017-09-18 22:33:33 +0300

    Show maintainers in 'Settings' page

---
+++ src/repo/settings/js/main.js
@@ -0,0 +1,19 @@
+repo.addMerger()
+	.then(() => {
+		return repo.getContent();
+	})
+	.then(content => {
+		showTitle(content.title);
+		showTabs(1);
+
+		return repo.getMaintainers();
+	})
+	.then(maintainers => {
+		let select = document.getElementById("maintainers_select");
+		select.innerHTML = "";
+		maintainers.forEach(maintainer => {
+			let option = document.createElement("option");
+			option.textContent = maintainer.name + "@zeroid.bit";
+			select.appendChild(option);
+		});
+	});
