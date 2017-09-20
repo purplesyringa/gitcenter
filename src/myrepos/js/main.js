40384e1106a31702bd3c4b0519705c05870bd27e
Ivanq <imachug@yandex.ru>
2017-09-20 13:07:05 +0300

    Add 'My repos' page

---
+++ src/myrepos/js/main.js
@@ -0,0 +1,52 @@
+let zeroFrame = new ZeroFrame();
+let zeroPage = new ZeroPage(zeroFrame);
+let zeroFS = new ZeroFS(zeroPage);
+let zeroDB = new ZeroDB(zeroPage);
+
+zeroPage.cmd("mergerSiteList")
+	.then(repos => {
+		repos = Object.keys(repos);
+
+		let index = repos.indexOf("1iNDExENNBsfHc6SKmy1HaeasHhm3RPcL");
+		if(index > -1) {
+			repos.splice(index, 1);
+		}
+
+		return Promise.all(
+			repos.map(repo => {
+				return zeroFS.readFile("merged-GitCenter/" + repo + "/content.json")
+					.then(content => {
+						content = JSON.parse(content);
+
+						return {
+							description: content.title + " (" + content.description + ")",
+							address: repo
+						};
+					});
+			})
+		);
+	})
+	.then(repos => {
+		repos.forEach(repo => {
+			let node = document.createElement("a");
+			node.className = "repo";
+			node.href = "/" + repo.address;
+
+			let title = document.createElement("div");
+			title.className = "repo-title";
+			title.textContent = repo.description;
+			node.appendChild(title);
+
+			let address = document.createElement("div");
+			address.className = "repo-address";
+			address.textContent = repo.address;
+			node.appendChild(address);
+
+			document.getElementById("repos").appendChild(node);
+		});
+
+		document.getElementById("download_repo").onclick = () => {
+			let example = location.protocol + "//" + location.host + "/1RepoXU8bQE9m7ssNwL4nnxBnZVejHCc6/";
+			zeroPage.alert("You can download a new repo by simply visiting its address, like <b>" + example + "</b>");
+		};
+	});
