3cf980357a75a7462e3b97de246870647fb0a12e
Ivanq <imachug@yandex.ru>
2017-09-20 18:27:19 +0300

    Add an empty PRs page

---
+++ src/repo/pull-requests/js/main.js
@@ -0,0 +1,16 @@
+if(address == "1RepoXU8bQE9m7ssNwL4nnxBnZVejHCc6") {
+	location.href = "../../default/";
+}
+
+repo.addMerger()
+	.then(() => {
+		return repo.getContent();
+	})
+	.then(content => {
+		if(!content.installed) {
+			location.href = "../../install/?" + address;
+		}
+
+		showTitle(content.title);
+		showTabs(1);
+	});
