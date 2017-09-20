a23d4c57f9a4564baa5e1a52002b6024ba9727e8
Ivanq <imachug@yandex.ru>
2017-09-20 09:46:23 +0300

    Repo: Sign correct file

--- src/js/repo.js
+++ src/js/repo.js
@@ -52,8 +52,8 @@ class Repository {
 			});
 	}
 	sign() {
-		return this.zeroPage.cmd("siteSign")
-			.cmd(res => {
+		return this.zeroPage.cmd("siteSign", {inner_path: "merged-GitCenter/" + this.address + "/content.json"})
+			.then(res => {
 				if(res != "ok") {
 					return Promise.reject(res);
 				}
