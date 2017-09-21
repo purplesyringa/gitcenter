e725e4c14ddd6aa9eda8b58bdc433e85adbc0bd0
Ivanq <imachug@yandex.ru>
2017-09-21 21:15:57 +0300

    Clone issues

--- src/repo/pull-requests/js/main.js
+++ src/repo/pull-requests/js/main.js
@@ -2,6 +2,8 @@ if(address == "1RepoXU8bQE9m7ssNwL4nnxBnZVejHCc6") {
 	location.href = "../../default/";
 }
 
+let currentPage = Number.isSafeInteger(+additional) ? +additional : 0;
+
 repo.addMerger()
 	.then(() => {
 		return repo.getContent();
@@ -13,4 +15,44 @@ repo.addMerger()
 
 		showTitle(content.title);
 		showTabs(1);
+		document.getElementById("new_pull_request").href = "new/?" + address;
+
+		additional = +additional;
+		return repo.getPullRequests(Number.isSafeInteger(additional) ? additional : 0);
+	})
+	.then(pullRequests => {
+		pullRequests.pullRequests.forEach(pullRequest => {
+			let tr = document.createElement("tr");
+			tr.onclick = () => {
+				location.href = "view/?" + address + "/" + pullRequest.id + "@" + pullRequest.json_id;
+			};
+
+			let title = document.createElement("td");
+			title.textContent = pullRequest.title;
+			tr.appendChild(title);
+
+			let icon = document.createElement("img");
+			icon.src = "../../img/pr-" + (pullRequest.open ? (pullRequest.reopened ? "reopened" : "open") : "closed") + ".svg";
+			icon.className = "pull-request-icon";
+			title.insertBefore(icon, title.firstChild);
+
+			let info = document.createElement("td");
+			info.textContent = "Opened on " + repo.translateDate(pullRequest.date_added) + " by " + pullRequest.cert_user_id;
+			info.className = "pull-requests-right";
+			tr.appendChild(info);
+
+			document.getElementById("pull_requests").appendChild(tr);
+		});
+
+		if(currentPage > 0) {
+			let button = document.getElementById("navigation_back");
+			button.classList.remove("button-disabled");
+			button.href = "?" + address + "/" + (currentPage - 1);
+		}
+
+		if(issues.nextPage) {
+			let button = document.getElementById("navigation_next");
+			button.classList.remove("button-disabled");
+			button.href = "?" + address + "/" + (currentPage + 1);
+		}
 	});
---
+++ src/repo/pull-requests/view/js/main.js
@@ -0,0 +1,125 @@
+if(address == "1RepoXU8bQE9m7ssNwL4nnxBnZVejHCc6") {
+	location.href = "../../../default/";
+}
+
+if(additional.indexOf("@") == -1) {
+	location.href = "../?" + address;
+}
+
+let id = parseInt(additional.substr(0, additional.indexOf("@")));
+let jsonId = parseInt(additional.substr(additional.indexOf("@") + 1));
+
+if(isNaN(id) || isNaN(jsonId)) {
+	location.href = "../?" + address;
+}
+
+function showComment(comment) {
+	let node = document.createElement("div");
+	node.className = "comment" + (jsonId == comment.json_id ? " comment-owned" : "");
+
+	let header = document.createElement("div");
+	header.className = "comment-header";
+	header.textContent = comment.cert_user_id + " commented on " + repo.translateDate(comment.date_added);
+	node.appendChild(header);
+
+	let content = document.createElement("div");
+	content.className = "comment-content";
+	content.textContent = comment.body;
+	node.appendChild(content);
+
+	document.getElementById("comments").appendChild(node);
+}
+
+function drawPullRequestStatus() {
+	let statusText = pullRequest.open ? (pullRequest.reopened ? "reopened" : "open") : "closed";
+
+	document.getElementById("pull_request_status").className = "pull-request-status pull-request-status-" + statusText;
+	document.getElementById("pull_request_status_img").src = "../../../img/pr-" + statusText + "-white.svg";
+	document.getElementById("pull_request_status_text").innerHTML = statusText[0].toUpperCase() + statusText.substr(1);
+
+	document.getElementById("comment_submit_close").innerHTML = "Comment and " + (pullRequest.open ? "close" : "reopen") + " pull request";
+}
+
+
+let pullRequest;
+repo.addMerger()
+	.then(() => {
+		return repo.getContent();
+	})
+	.then(content => {
+		if(!content.installed) {
+			location.href = "../../../install/?" + address;
+		}
+
+		showTitle(content.title);
+		showTabs(2);
+
+		return repo.getPullRequest(id, jsonId);
+	})
+	.then(i => {
+		pullRequest = i;
+
+		document.getElementById("pull_request_title").textContent = pullRequest.title;
+		document.getElementById("pull_request_id").textContent = id;
+		document.getElementById("pull_request_json_id").textContent = jsonId;
+
+		drawPullRequestStatus();
+
+		return repo.getPullRequestComments(id, jsonId);
+	})
+	.then(comments => {
+		comments.forEach(showComment);
+
+		document.getElementById("comment_submit").onclick = () => {
+			let contentNode = document.getElementById("comment_content");
+			if(contentNode.disabled || contentNode.value == "") {
+				return;
+			}
+
+			contentNode.disabled = true;
+
+			repo.addPullRequestComment(id, jsonId, contentNode.value)
+				.then(comment => {
+					showComment(comment);
+
+					contentNode.value = "";
+					contentNode.disabled = false;
+				});
+		};
+
+		document.getElementById("comment_submit_close").onclick = () => {
+			let contentNode = document.getElementById("comment_content");
+			if(contentNode.disabled) {
+				return;
+			}
+
+			contentNode.disabled = true;
+
+			let promise;
+			if(contentNode.value == "") {
+				promise = Promise.resolve();
+			} else {
+				promise = repo.addPullRequestComment(id, jsonId, contentNode.value)
+					.then(comment => {
+						showComment(comment);
+					});
+			}
+
+			promise
+				.then(() => {
+					return repo.changePullRequestStatus(id, jsonId, !pullRequest.open);
+				})
+				.then(() => {
+					if(pullRequest.open) {
+						pullRequest.open = false;
+					} else {
+						pullRequest.open = true;
+						pullRequest.reopened = true;
+					}
+					drawPullRequestStatus();
+
+					contentNode.value = "";
+					contentNode.disabled = false;
+				});
+		};
+	});
