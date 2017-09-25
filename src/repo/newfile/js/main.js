d9e15832db97584f2c02fc2ef0537939e4611177
Ivanq <imachug@yandex.ru>
2017-09-25 16:42:38 +0300

    Add 'New file' button

---
+++ src/repo/newfile/js/main.js
@@ -0,0 +1,41 @@
+if(address == "1RepoXU8bQE9m7ssNwL4nnxBnZVejHCc6") {
+	location.href = "../../default/";
+}
+
+repo.addMerger()
+	.then(() => {
+		return repo.getContent();
+	})
+	.then(content => {
+		if(!content.installed) {
+			location.href = "../../install/?" + address;
+		}
+
+		showTitle(content.title);
+		showBranches();
+		showPath(true);
+		showLinks();
+		showTabs(1);
+
+		document.getElementById("cancel").href = "../?" + address + "/" + path.replace(/@/g, "@@") + "@" + branch.replace(/@/g, "@@");
+
+		let fileContent = document.getElementById("file_content");
+		let fileName = document.getElementById("file_name");
+		let saveButton = document.getElementById("save");
+		let commitMessage = document.getElementById("commit_message");
+		saveButton.onclick = () => {
+			if(saveButton.classList.contains("button-disabled")) {
+				return;
+			}
+			if(commitMessage.value == "" || fileName.value == "") {
+				return;
+			}
+
+			saveButton.classList.add("button-disabled");
+
+			repo.saveFile((path ? path + "/" : "") + fileName.value, repo.git.stringToArray(fileContent.value), branch, commitMessage.value)
+				.then(commit => {
+					location.href = "../edit/?" + address + "/" + ((path ? path + "/" : "") + fileName.value).replace(/@/g, "@@") + "@" + (repo.git.isSha(branch) ? commit : branch).replace(/@/g, "@@");
+				});
+		};
+	});
