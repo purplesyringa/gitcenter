9580e905ab99af8ad3884972f2caba8522d03e19
Ivanq <imachug@yandex.ru>
2017-09-30 16:43:12 +0300

    Some fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,31 @@
 let zeroFrame = new ZeroFrame();
 let zeroPage = new ZeroPage(zeroFrame);
 
+let siteInfo, list;
 zeroPage.getSiteInfo()
-	.then(siteInfo => {
+	.then(s => {
+		siteInfo = s;
 		if(siteInfo.settings.permissions.indexOf("Merger:GitCenter") == -1) {
 			return zeroPage.cmd("wrapperPermissionAdd", ["Merger:GitCenter"]);
+		}
+	})
+	.then(() => {
+		if(siteInfo.settings.permissions.indexOf("Cors:1iD5ZQJMNXu43w1qLB8sfdHVKppVMduGz") == -1) {
+			return zeroPage.cmd("corsPermission", ["1iD5ZQJMNXu43w1qLB8sfdHVKppVMduGz"]);
+		}
+	})
+	.then(() => {
+		return zeroPage.cmd("mergerSiteList");
+	})
+	.then(l => {
+		list = l;
+		if(!list["1RepoXU8bQE9m7ssNwL4nnxBnZVejHCc6"]) {
+			return zeroPage.cmd("mergerSiteAdd", ["1RepoXU8bQE9m7ssNwL4nnxBnZVejHCc6"]);
+		}
+	})
+	.then(() => {
+		if(!list["1iNDExENNBsfHc6SKmy1HaeasHhm3RPcL"]) {
+			return zeroPage.cmd("mergerSiteAdd", ["1iNDExENNBsfHc6SKmy1HaeasHhm3RPcL"]);
 		}
 	})
 	.then(() => {
