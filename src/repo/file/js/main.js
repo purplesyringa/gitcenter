a4315cc7034a3c6a89a7a56ada19714833c97985
Ivanq <imachug@yandex.ru>
2017-10-12 22:30:11 +0300

    Show commit in /repo/file

--- src/repo/file/js/main.js
+++ src/repo/file/js/main.js
@@ -2,6 +2,8 @@ if(address == "1RepoXU8bQE9m7ssNwL4nnxBnZVejHCc6") {
 	location.href = "../../default/";
 }
 
+let head;
+
 repo.addMerger()
 	.then(() => {
 		return repo.getContent();
@@ -20,7 +22,17 @@ repo.addMerger()
 
 		document.getElementById("edit").href = "../edit/?" + address + "/" + path.replace(/@/g, "@@") + "@" + branch.replace(/@/g, "@@");
 
-		return repo.getFiles(branch, "");
+		return branch || repo.git.getHead();
+	})
+	.then(h => {
+		head = h;
+		return repo.git.readBranchCommit(head);
+	})
+	.then(commit => {
+		document.getElementById("commit_title").textContent = commit.content.message;
+		document.getElementById("commit_description").textContent = repo.parseAuthor(commit.content.committer);
+
+		return repo.getFiles(head, "");
 	})
 	.then(() => {
 		// Tree exists
