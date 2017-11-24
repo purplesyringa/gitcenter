dfffcec2509b93ef6e0141e44b870e4b1decdb29
Ivanq <imachug@yandex.ru>
2017-11-24 16:15:50 +0300

    Add 'type' select

--- src/install/js/main.js
+++ src/install/js/main.js
@@ -11,29 +11,48 @@ repo.addMerger()
 		let description = document.getElementById("description");
 		let gitAddress = document.getElementById("address");
 
-		title.oninput = () => {
+		let showGitAddress = () => {
 			gitAddress.value = title.value.replace(/[^a-zA-Z0-9]/g, "-").replace(/-+/g, "-").replace(/^-|-$/g, "").toLowerCase();
 			if(gitAddress.value) {
-				gitAddress.value += ".git";
+				if(typeSelect.value == "git") {
+					gitAddress.value += ".git";
+				} else {
+					gitAddress.value += "/.hg";
+				}
+			} else {
+				if(typeSelect.value == "git") {
+					gitAddress.value = "repo.git";
+				} else {
+					gitAddress.value = "repo/.hg";
+				}
 			}
+		};
+
+		title.oninput = () => {
+			showGitAddress();
 		};
 
 		let installButton = document.getElementById("install");
 		installButton.onclick = () => {
 			if(installButton.classList.contains("button-disabled")) {
 				return;
-			} else if(title.value == "" || description.value == "" || gitAddress == "") {
+			} else if(title.value == "" || description.value == "" || gitAddress.value == "") {
 				return;
 			}
 
 			installButton.classList.add("button-disabled");
 
-			repo.install(title.value, description.value, gitAddress.value)
+			repo.install(title.value, description.value, gitAddress.value, typeSelect.value)
 				.then(() => {
 					location.href = "../repo/?" + address;
 				}, e => {
 					zeroPage.error(e);
 					installButton.classList.remove("button-disabled");
 				});
+		};
+
+		let typeSelect = document.getElementById("type");
+		typeSelect.onchange = () => {
+			showGitAddress();
 		};
 	});
