7f6bd2aee407edcb5653d456ce22cb91d9513d0c
Ivanq <imachug@yandex.ru>
2017-12-07 23:14:57 +0300

    Fix

--- src/js/repo.js
+++ src/js/repo.js
@@ -1757,7 +1757,7 @@ class Repository {
 			return "1 hour ago";
 		} else if(delta < 60* 60 * 24) {
 			return Math.floor(delta / 60 / 60) + " hours ago";
-		} else if(delta < day * 2) {
+		} else if(delta < 60 * 60 * 24 * 2) {
 			return "yesterday";
 		} else {
 			let day = a.getDate();
