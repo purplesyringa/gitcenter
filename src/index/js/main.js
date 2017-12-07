f9d551796372e721251aeb1737eadfe1a3309454
Ivanq <imachug@yandex.ru>
2017-12-07 14:40:38 +0300

    Show star count as div

--- src/index/js/main.js
+++ src/index/js/main.js
@@ -2,7 +2,15 @@ zeroFrame = new ZeroFrame();
 zeroPage = new ZeroPage(zeroFrame);
 zeroDB = new ZeroDB(zeroPage);
 
-zeroDB.query("SELECT repo_index.*, json.cert_user_id FROM repo_index, json WHERE repo_index.json_id = json.json_id")
+zeroDB.query("\
+	SELECT repo_index.*, json.cert_user_id, COUNT(repo_stars.address) AS stars\
+	FROM repo_index, json\
+	LEFT JOIN repo_stars ON repo_stars.address = repo_index.address\
+	\
+	WHERE repo_index.json_id = json.json_id\
+	GROUP BY repo_index.address\
+	ORDER BY stars DESC\
+")
 	.then(index => {
 		index.forEach(repo => {
 			let node = document.createElement("a");
@@ -13,6 +21,11 @@ zeroDB.query("SELECT repo_index.*, json.cert_user_id FROM repo_index, json WHERE
 			title.className = "repo-title";
 			title.textContent = repo.title;
 			node.appendChild(title);
+
+			let stars = document.createElement("div");
+			stars.className = "repo-stars";
+			stars.textContent = repo.stars;
+			node.appendChild(stars);
 
 			let address = document.createElement("div");
 			address.className = "repo-address";
