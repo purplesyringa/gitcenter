57b303eb14802ffd2af0e4f0238338bb1ec87a79
Ivanq <imachug@yandex.ru>
2017-12-21 22:25:03 +0300

    Hg: Empty tree 4b825dc642cb6eb9a060e54bf8d69288fbee4904

--- src/js/hg.js
+++ src/js/hg.js
@@ -289,6 +289,14 @@ class Hg {
 			});
 	}
 	readManifest(sha) {
+		if(sha == "4b825dc642cb6eb9a060e54bf8d69288fbee4904") {
+			// Empty tree from Git. How did it appear here?
+			return Promise.resolve({
+				type: "tree",
+				content: []
+			});
+		}
+
 		let index, rev, metaData;
 
 		return this.loadIndex("store/00manifest")
