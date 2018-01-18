8c355b6ba94bd2789e32019dfa942554113520d4
Ivanq <imachug@yandex.ru>
2018-01-18 16:26:22 +0300

    Add VueRouter

---
+++ main/src/libs/vuerouter.js
@@ -0,0 +1,17 @@
+import Vue from "vue/dist/vue.min.js";
+import Router from "./router.js";
+
+export default zeroPage => {
+	const router = new Router(zeroPage);
+
+	const plugin = {
+		router,
+		install: () => {
+			Object.defineProperty(Vue.prototype, "$router", {
+				get: () => router
+			})
+		}
+	};
+
+	return plugin;
+};
