8993b6ab019105088eb00518c63c48fd97c4dbb3
Ivanq <imachug@yandex.ru>
2018-01-19 20:54:17 +0300

    Return router and plugin from VueRouter constructor

--- main/src/libs/vuerouter.js
+++ main/src/libs/vuerouter.js
@@ -13,5 +13,5 @@ export default zeroPage => {
 		}
 	};
 
-	return plugin;
+	return {plugin, router};
 };
--- main/src/route.js
+++ main/src/route.js
@@ -3,7 +3,8 @@ const zf = new ZeroFrame();
 
 import Vue from "vue/dist/vue.min.js";
 import VueRouter from "./libs/vuerouter.js";
-Vue.use(VueRouter(zf));
+const router = VueRouter(zf);
+Vue.use(router.plugin);
 
 import Home from "./router_pages/home/home.vue";
 
