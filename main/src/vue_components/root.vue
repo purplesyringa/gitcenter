aadb56c8582e26ba4ad18d738fa6499b57541356
Ivanq <imachug@yandex.ru>
2018-01-19 19:40:17 +0300

    Load homepage in root.vue

--- main/src/vue_components/root.vue
+++ main/src/vue_components/root.vue
@@ -1,19 +1,30 @@
 <template>
 	<div>
 		<gc-header />
-		<component :is="currentView"></component>
+
+		<div class="current-view">
+			<component :is="currentView"></component>
+		</div>
 	</div>
 </template>
 
+<style type="text/css">
+.current-view {
+	display: block;
+	width: 896px;
+	margin: 0 calc(50% - 448px);
+}
+</style>
+
 <script language="text/javascript">
-	import Header from "./gc-header/header.vue";
+	import Home from "../router_pages/home.vue";
 
 	export default {
 		props: [],
 		name: "root",
 		data: () => {
 			return {
-				currentView: Header
+				currentView: Home
 			};
 		}
 	};
