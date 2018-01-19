f55038a083dfb0377218bad957f7b6fbe3ca1501
Ivanq <imachug@yandex.ru>
2018-01-19 19:39:52 +0300

    Init home page

--- main/src/router_pages/home.vue
+++ main/src/router_pages/home.vue
@@ -1,13 +1,33 @@
 <template>
-	<gc-header />
+	<div>
+		<h1>Welcome to Git Center!</h1>
+		<p>
+			<b>Git Center</b> is a decentralized hosting platform for Git repositories. We provide several collaboration features such as bug tracking for every project, and private and public repositories for free. Join us by creating a new repository.
+		</p>
+		<p>
+			<b>Git Center</b> is built using ZeroNet, so its workflow quite differs from GitHub. If you have any questions, <a href="support/">ask us for support</a>.
+		</p>
+		<p>
+			<br>
+			<a class="button" href="guide/">Beginner's Guide</a>
+			<a id="create_repository" class="button button-blue button-disabled">New repository</a>
+		</p>
+
+		<h2>Contributors</h2>
+		<p>
+			If I missed you, mail me <a href="/Mail.ZeroNetwork.bit/?to=gitcenter">here</a>.
+		</p>
+		<ul>
+			<li>Krixano - contibutor, tester</li>
+			<li>Binchan - contibutor, tester</li>
+			<li>ixhb5a7vwvdgimjx - beta-testing</li>
+			<li>affeali - domain</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 	export default {
-		props: [],
-		name: "mainapp",
-		data: () => {
-			return {};
-		}
+		name: "home"
 	};
 </script>
