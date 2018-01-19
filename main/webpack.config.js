6cd94a6b4fbfade302e6c34fa91298b3cd68c4a8
Ivanq <imachug@yandex.ru>
2018-01-19 22:54:33 +0300

    Add simple config

---
+++ main/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+const webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/main.js",
+	output: {
+		path: path.resolve(__dirname, "./dist"),
+		publicPath: "/dist/",
+		filename: "build.js"
+	},
+	module: {
+		rules: []
+	}
+};
