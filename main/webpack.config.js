1a88df9a2b615dd0c4fe39362d7ab143c09ce9f3
Ivanq <imachug@yandex.ru>
2018-01-20 10:09:25 +0300

    Add HTML

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
 
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: "./src/main.js",
 	output: {
 		path: path.resolve(__dirname, "./dist"),
-		publicPath: "/dist/",
+		publicPath: "./",
 		filename: "build.js"
 	},
 	module: {
@@ -25,5 +26,15 @@ module.exports = {
 				exclude: /node_modules/
 			}
 		]
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Git Center",
+			template: "./src/index.html",
+			seo: {
+				keywords: "gitcenter,git,repository",
+				description: "GitHub for ZeroNet"
+			}
+		})
+	]
 };
