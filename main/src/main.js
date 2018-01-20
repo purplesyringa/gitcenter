0d5e97ad3b4a15958bd3cc73b66b22d2ac2fe55a
Ivanq <imachug@yandex.ru>
2018-01-20 10:10:37 +0300

    Move Vue import to the top

--- main/src/main.js
+++ main/src/main.js
@@ -1,7 +1,7 @@
+import Vue from "vue/dist/vue.min.js";
 import Header from "./vue_components/gc-header/header.vue";
 Vue.component("gc-header", Header);
 
-import Vue from "vue/dist/vue.min.js";
 import root from "./vue_components/root.vue";
 var app = new Vue({
 	el: "#app",
