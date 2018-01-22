74b974053c5a966964a74f8964f68a020c99e3d6
Ivanq <imachug@yandex.ru>
2018-01-22 21:56:18 +0300

    Create ZeroDB in route.js

--- main/src/route.js
+++ main/src/route.js
@@ -4,6 +4,9 @@ const zf = new ZeroFrame();
 import ZeroPage from "zero-dev-lib/ZeroPage";
 const zp = new ZeroPage(zf);
 
+import ZeroDB from "zero-dev-lib/ZeroDB";
+const zeroDB = new ZeroDB(zp);
+
 import Vue from "vue/dist/vue.min.js";
 import VueRouter from "./libs/vuerouter.js";
 const router = VueRouter(zp);
@@ -17,4 +20,4 @@ export const route = vue => {
 	router.router.check(router.router.getURL());
 };
 
-export {zp as zeroPage};
+export {zp as zeroPage, zeroDB};
