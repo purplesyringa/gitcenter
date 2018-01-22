84309c8ac8ce05512cb5bb2cfa17d9e295b88a1f
Ivanq <imachug@yandex.ru>
2018-01-22 21:55:47 +0300

    Add vue-async-computed plugin

--- main/src/main.js
+++ main/src/main.js
@@ -3,6 +3,9 @@ import "./sass/buttons.sass";
 
 import Vue from "vue/dist/vue.min.js";
 
+import AsyncComputed from "vue-async-computed";
+Vue.use(AsyncComputed);
+
 import root from "./vue_components/root.vue";
 var app = new Vue({
 	el: "#app",
